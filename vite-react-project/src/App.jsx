54480e3454e6ac597fb0ff34999649c289a4ad31
Andrey Yurov <ayurov20@gmail.com>
2023-10-10 20:19:44 +0300

    khb

--- vite-react-project/src/App.jsx
+++ vite-react-project/src/App.jsx
@@ -1,16 +1,23 @@
-import './index.module.css'
+import { useState } from 'react'
+import './index.scss'
 
 function App() {
- 
+ const [count, setCount] = useState(0)
 
+ function increaseCount () {
+  setCount(count + 1)
+ }
+ function decreaseCount () {
+  setCount(count - 1)
+ }
   return (
     <>
       <div className='App'>
         <div>
           <h2>Счетчик:</h2>
-          <h1>0</h1>
-          <button className='minus'>Минус - </button>
-          <button className='plus'>Плюс + </button>
+          <h1>{count}</h1>
+          <button onClick={decreaseCount} className='minus'>Минус - </button>
+          <button onClick={increaseCount} className='plus'>Плюс + </button>
         </div>
       </div>
         
