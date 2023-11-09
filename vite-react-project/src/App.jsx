962c013225c9c1fbac6ba281f6b6a45564d01b91
Andrey Yurov <ayurov20@gmail.com>
2023-11-09 16:09:41 +0300

    пр

--- vite-react-project/src/App.jsx
+++ vite-react-project/src/App.jsx
@@ -1,7 +1,19 @@
 import Block from "./Components/Block"
 import './App.scss'
+import { useEffect, useState } from "react"
 
 function App() {
+  const [rates, setRates] = useState({})
+
+  useEffect(()=>{
+    fetch('https://www.cbr-xml-daily.ru/latest.js').then((res)=>res.json()).then((json)=>{
+      setRates(json.rates)
+      console.log(json.rates)
+    }).catch((err)=>{
+      console.warn(err)
+      alert('Не удалось получить информацию!')
+    })
+  }, [])
 
   return (
     <div className='App'>
