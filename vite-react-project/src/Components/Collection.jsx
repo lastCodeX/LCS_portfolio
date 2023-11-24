704484addee974dd272d0d15705c1fa35f510409
Andrey Yurov <ayurov20@gmail.com>
2023-11-24 10:57:01 +0300

    zz

---
+++ vite-react-project/src/Components/Collection.jsx
@@ -0,0 +1,17 @@
+import './Collection.scss'
+
+const Collection = (name, images) => {
+    return (
+        <div className="collection">
+            <img className="collection_big" src={images[0]} alt="collection_big_img"/>
+            <div className="collection_bottom">
+                <img className="collection_mini" src={images[1]} alt="Item"/>
+                <img className="collection_mini" src={images[2]} alt="Item"/>
+                <img className="collection_mini" src={images[3]} alt="Item"/>
+            </div>
+            <h4>{name}</h4>
+        </div>
+    )
+}
+
+export default Collection
