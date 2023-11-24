4105fa4440522440e1a0519bdf093964486a1a00
Andrey Yurov <ayurov20@gmail.com>
2023-11-24 10:12:57 +0300

    d

--- vite-react-project/src/App.jsx
+++ vite-react-project/src/App.jsx
@@ -1,9 +1,10 @@
 import Block from "./Components/Block"
 import './App.scss'
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef} from "react"
 
 function App() {
-  const [rates, setRates] = useState({})
+  // const [rates, setRates] = useState({})
+  const ratesRef = useRef({})
   const [fromCurrency, setFromCurrency] = useState('RUB')
   const [toCurrency, setToCurrency] = useState('USD')
   const [fromPrice, setFromPrice] = useState(0)
@@ -12,24 +13,33 @@ function App() {
   useEffect(() => {
     fetch('https://www.cbr-xml-daily.ru/latest.js').then((res) => res.json()).then((json) => {
       json.rates['RUB'] = 1
-      setRates(json.rates)
-      console.log(json.rates)
+      // setRates(json.rates)
+      ratesRef.current = json.rates
+      onChangeToPrice(1)
     }).catch((err) => {
       console.warn(err)
       alert('Не удалось получить информацию!')
     })
   }, [])
 
+  useEffect(()=>{
+    onChangeFromPrice(fromPrice)
+
+  }, [fromCurrency, fromPrice])
+
+  useEffect(()=>{
+    onChangeToPrice(toPrice)
+  }, [toCurrency, toPrice])
+
   const onChangeFromPrice = (value) => {
-    const price = value * rates[fromCurrency]
-    const result = price / rates[toCurrency]
+    const price = value / ratesRef.current[fromCurrency]
+    const result = price * ratesRef.current[toCurrency]
     setToPrice(result)
     setFromPrice(value)
   }
 
   const onChangeToPrice = (value) => {
-    const price = value * rates[toCurrency]
-    const result = price / rates[fromCurrency]
+    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value
     setFromPrice(result)
     setToPrice(value)
   }
